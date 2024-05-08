e1aed318208dc9f83323e9ac099d821810416c88
rizalsy17 <syahfirst51@gmail.com>
2024-05-08 12:26:46 +0700

    make error test

--- __test__/redux/authSlice.test.js
+++ __test__/redux/authSlice.test.js
@@ -97,7 +97,7 @@ describe('authSlice', () => {
 
         it('should return dataAsyncMe, loadingAsyncMe & errorAsyncMe state when given by asyncMe action', () => {
             const initialState = {};
-            const action = { type: asyncMesafadfasfs.fulfilled, payload: fakeUser };
+            const action = { type: asyncMe.fulfilled, payload: fakeUser };
 
             const nextState = authReducer(initialState, action);
 
